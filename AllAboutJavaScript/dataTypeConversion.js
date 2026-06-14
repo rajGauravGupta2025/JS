c16bb7ff3456b99a22cf0271083df6d784f04ff1
Raj Gaurav Gupta <149098451+rajGauravGupta2025@users.noreply.github.com>
2026-06-14 07:05:57 +0000

    Data types conversion

---
+++ AllAboutJavaScript/dataTypeConversion.js
@@ -0,0 +1,86 @@
+let score = 34
+// console.log(typeof score)
+// console.log(typeof(score))
+
+score = "33"
+// console.log(typeof score)
+// console.log(typeof(score))
+
+//console.log(typeof convertToNumber)
+
+// console.log('Checking again...................')
+score = "34fchu"
+let convertToNumber = Number(score)
+// console.log(typeof score)
+// console.log(typeof convertToNumber)
+// console.log(convertToNumber)
+
+let randomNumber = 99
+let stringNumber = String(randomNumber)
+// console.log(randomNumber)
+// console.log(stringNumber);
+// console.log(typeof randomNumber);
+// console.log(typeof stringNumber);
+
+
+/* -------------------Operations------------------------*/
+let value = 5
+let negativeValue = -value
+// console.log(value);
+// console.log(negativeValue);
+
+// console.log(2+5);
+// console.log(2-5);
+// console.log(2*5);
+// console.log(3/5);
+// console.log(5%9);
+
+let str1 = "Hello, "
+let str2 = 'Raj'
+let str = str1 + str2
+// console.log(typeof str)
+// console.log(str);
+
+// console.log("1"+2)
+// console.log(1+"2");
+
+//-----Note: Adding string to number or number to string the output is always a string...-----
+// console.log("1"+2+2);
+// console.log(2+4+"3")          //Here is the twist, you need to note this............
+
+//Tricky conversion..................
+// console.log(true);
+// console.log(+true);
+// console.log(+"");
+
+//JavaScript understands that 2 is a string and while comparing it with 1, it converts it into integer..
+// console.log("2" > 1);
+// console.log("02" >1);
+
+//Always remember this..................& always try to avoid this............
+// console.log(null == 0);
+// console.log(null >0);
+// console.log(null >= 0);
+
+console.log(undefined);
+console.log(undefined ==0);
+console.log(undefined >0);
+console.log(undefined >=0);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
