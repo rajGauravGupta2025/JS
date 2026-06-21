a0b5dae8367ed374092ae093d53831d6913aa78d
Raj Gaurav Gupta <149098451+rajGauravGupta2025@users.noreply.github.com>
2026-06-21 06:40:47 +0000

    Loops

---
+++ AllAboutJavaScript/loops.js
@@ -0,0 +1,26 @@
+//for-loop....
+for(let idx=0;idx<10;idx++){
+    const element = idx;
+    console.log(element);
+}
+
+//loop in array....
+const myArray = ["Raj","Shivam","Dev","Shivansh"]
+for(let i=0;i<myArray.length;i++){
+    console.log(myArray[i])
+}
+
+//while loop...........
+let index = 0;
+while(index<=10){
+    let element = index*2
+    console.log(element)
+    index = index+1
+}
+
+//do-while loop....
+let i=0
+do{
+    console.log(i)
+    i++;
+}while(i<=10)
