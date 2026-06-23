f84dd9fc9842a9b9823acff34f10f168fc304204
Raj Gaurav Gupta <149098451+rajGauravGupta2025@users.noreply.github.com>
2026-06-23 08:19:49 +0000

    Digital Clock

---
+++ AllAboutJavaScript/DigitalClock/script.js
@@ -0,0 +1,6 @@
+const clock = document.getElementById('clock');
+//const clock = document.querySelector('#clock)
+setInterval(function () {
+  let date = new Date().toLocaleTimeString();
+  clock.innerHTML = date;
+}, 1000);
